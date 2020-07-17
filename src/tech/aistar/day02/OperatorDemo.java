0abeb2e7752825a6bbf8a6a9daeb886b559bba43
guancg <849962874@qq.com>
2020-07-17 16:00:51 +0800

    逻辑运算符

--- src/tech/aistar/day02/OperatorDemo.java
+++ src/tech/aistar/day02/OperatorDemo.java
@@ -40,16 +40,68 @@ public class OperatorDemo {
 
         //练习
         int t1 = 1;
-        int t2 = t1++ + ++t1 + --t1;
+
+        //变量    t1  2    3   2
+
+        //表达式      1    3    2 =
+
+        int t2 = t1++ + ++t1 + --t1;//t2 = 6
+
+        //变量    t2(5)  t1()
+        //表达式   6      3    6 = 15
         t2 = t2-- + ++t1 + ++t2;
-        System.out.println("t2:"+t2);
+        System.out.println("t2:"+t2);//15
+
+        //尽量不要使用
+        //拓展的 ++ -- 底层会进行窄化操作[强制类型转换]
+        byte b = 127;
+
+        //java中的数据都是带有符号的数据
+        //最高为是符号位 0-正数,1-负数
+        //b = (byte) (b + 1);
+
+        ++b;//编译是ok的,底层->b = (byte) (b + 1);
+
+        System.out.println(b);// -128
+
+        char c = 'a';
+        c++;//c = (char)(c + 1);
+        System.out.println(c);//b
+
+        double pi = 3.14;
+        pi++;//pi = pi + 1;
+        System.out.println(pi);//4.140000000000001
 
 
+        int a = 1;
+        int ts = a+=2;//a = a + 2;
+        System.out.println("a:"+a);
+        System.out.println("ts:"+ts);//3 +=  优先级 高于 =
 
+        // += 也是要慎用的. - 底层也是会进行窄化操作的
+        short s1 = 1;
+        //s1 = (short) (s1 + 1);
+        s1+=1;//ok - s1 = (short) (s1 + 1);
+        System.out.println("s1:"+s1);
 
+        System.out.println("===比较运算符==");
+        System.out.println(10 == 10);//true
 
+        //基本数据类型,== 就是比较变量中保存的数据.
 
+        System.out.println(2 > 1);//true
 
+        System.out.println("====华丽丽的分割线====");
+        //逻辑与
+        //变量或者字面量以及比较运算符组成的合法的语句 - 条件表达式
+        System.out.println(1>0 && 2>1);//true
 
+        boolean flag  = true;
+        System.out.println(flag && true);//ok
+
+        //证明逻辑与的短路的特性.
+        int b1 = 1;
+        System.out.println(false && (b1++)>0);
+        System.out.println("b1:"+b1);//1
     }
 }
