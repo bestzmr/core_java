1badc773617be1111dcf550cd1859a20292a650f
guancg <849962874@qq.com>
2020-07-20 13:43:58 +0800

    方法的重载

---
+++ src/tech/aistar/day03/overloads/LoadDemo.java
@@ -0,0 +1,38 @@
+package tech.aistar.day03.overloads;
+
+/**
+ * 本类用来演示:重载的方法
+ * 前提:重载的方法肯定是出现在同一个类中
+ * 1. 重载的方法的名称肯定是一致的.
+ * 2. 重载的方法的参数列表肯定是不一样的
+ *    2-1. 参数列表的个数不一样
+ *    2-2. 个数一样的时候,参数类型肯定要不一样
+*  3. 重载的方法的返回类型可以不一样
+ *
+ * @author: success
+ * @date: 2020/7/20 1:34 下午
+ */
+public class LoadDemo {
+    public static void main(String[] args) {
+        add(10.0d);
+
+        System.out.println("ok");
+    }
+
+    public static void add(){
+        System.out.println("add");
+    }
+
+    public static void add(int i){
+        System.out.println("i:"+i);
+    }
+
+    public static void add(int i,String j){
+        System.out.println("i:"+i+",j:"+j);
+    }
+
+    public static double add(double d){
+        System.out.println("double:"+d);
+        return d;
+    }
+}
