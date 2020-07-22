8e88ca8e5a7113aafd25ca0beafc179dc6a1c6f1
guancg <849962874@qq.com>
2020-07-22 10:45:31 +0800

    数组

--- src/tech/aistar/day05/ArrayDemo.java
+++ src/tech/aistar/day05/ArrayDemo.java
@@ -23,6 +23,19 @@ public class ArrayDemo {
             arr[i] = (int) (Math.random()*100+1);
         }
 
+        //3. 可以在定义数组的同时进行赋值
+        int[] arr2 = {12,55,66};
+
+        //3-1.
+        int[] arr3 = new int[]{100,200,300};
+
+        //4. 可以先声明一个数组,然后再进行统一赋值.
+        int[] arr4 = new int[3];
+
+        //统一赋值
+        arr4 = new int[]{900,800,700};
+        //arr4 = {222,333,222};//error
+
         //java.lang.ArrayIndexOutOfBoundsException - 数组下标越界异常.
         //System.out.println(arr[3]);
 
@@ -41,5 +54,20 @@ public class ArrayDemo {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+
+        System.out.println("=====");
+        //3.jdk5.0以后提供的foreach语法 - 增强for循环(只读的for)
+        for (int i : arr2) {//i不是代表下标,就是代表实际的遍历到的数组中的元素
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * 自定义数组的数组方式 - [想,想,想]
+     * @param arr
+     * @return
+     */
+    public static String toArrayString(int[] arr){
+        return null;
     }
 }
