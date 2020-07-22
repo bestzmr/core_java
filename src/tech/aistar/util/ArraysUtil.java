ea60a12f40044f5b132bf17254f3cc793b3d8214
guancg <849962874@qq.com>
2020-07-22 14:33:05 +0800

    自定义数组工具类

---
+++ src/tech/aistar/util/ArraysUtil.java
@@ -0,0 +1,78 @@
+package tech.aistar.util;
+
+/**
+ * 本类用来演示:数组工具类
+ *
+ * @author: success
+ * @date: 2020/7/22 2:22 下午
+ */
+public class ArraysUtil {
+
+    /**
+     * 找出数组中最大的值.
+     * @param arr
+     * @return
+     */
+    public static int getMaxElement(int[] arr){
+        return -1;
+    }
+
+    /**
+     * 找出数组arr中target元素对应的所有的下标
+     * @param arr
+     * @param target 元素
+     * @return
+     */
+    public static int[] getIndexByTarget(int[] arr,int target){
+        return null;
+    }
+
+    /**
+     * 根据下标删除数组中的元素
+     * @param arr
+     * @param index 下标是唯一的
+     * @return
+     */
+    public static int[] delByIndex(int[] arr,int index){
+        return null;
+    }
+
+    /**
+     * 根据元素进行删除
+     *
+     * 删除数组中所有的target元素.
+     * @param arr
+     * @param target
+     * @return
+     */
+    public static int[] delByTarget(int[] arr,int target){
+        return null;
+    }
+
+    /**
+     * 数组排重 - 去除数组中重复的数据,只保留1个.
+     * @param arr
+     * @return
+     */
+    public static int[] delDoubleElement01(int[] arr){
+        return null;
+    }
+
+    /**
+     * 数组排重 - 去除数组中重复的数据,只保留1个.
+     * @param arr
+     * @return
+     */
+    public static int[] delDoubleElement02(int[] arr){
+        return null;
+    }
+
+    /**
+     * 数组排重 - 去除数组中重复的数据,只保留1个.
+     * @param arr
+     * @return
+     */
+    public static int[] delDoubleElement03(int[] arr){
+        return null;
+    }
+}
