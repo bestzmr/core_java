c0a2ffde0ccf2fbf55880e62856934202ec5da5c
guancg <849962874@qq.com>
2020-07-29 09:18:20 +0800

    作业

--- src/tech/aistar/day08/relation/homework/TestShapeDemo.java
+++ src/tech/aistar/day08/relation/homework/TestShapeDemo.java
@@ -8,9 +8,32 @@ package tech.aistar.day08.relation.homework;
  */
 public class TestShapeDemo {
     public static void main(String[] args) {
-        Shape square = new Square(10);
-        System.out.println(square.area());
+        //创建一个形状数组
+        Shape[] shapes = new Shape[3];
 
-        System.out.println(square.girth());
+        //多态的应用
+        //对象的编译时类型写成父类,对象的运行时类型写成子类
+        Shape square = new Square(10);
+
+        Shape rect = new Rect(10,20);
+
+        Shape circle = new Circle(10.0d);
+
+        shapes[0] = square;
+        shapes[1] = rect;
+        shapes[2] = circle;
+
+        for (Shape s : shapes) {
+            System.out.println(s.area());
+            System.out.println(s.girth());
+            System.out.println("===");
+        }
+
+       // System.out.println(0.1+0.2);
+
+        //System.out.println(((1.0 + 2.0)/10));
+
+        //System.out.println((2*314.0*10.0)/100);
+
     }
 }
