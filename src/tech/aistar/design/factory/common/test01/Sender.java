af9472a24e10f04185cbefccf2fd027eb876bfe3
guancg <849962874@qq.com>
2020-08-03 10:17:33 +0800

    普通简单工厂

---
+++ src/tech/aistar/design/factory/common/test01/Sender.java
@@ -0,0 +1,10 @@
+package tech.aistar.design.factory.common.test01;
+
+/**
+ * 本类用来演示:
+ *
+ * @author: success
+ * @date: 2020/8/3 9:57 上午
+ */
+public interface Sender {
+}
---
+++ src/tech/aistar/design/factory/common/test01/WXSender.java
@@ -0,0 +1,13 @@
+package tech.aistar.design.factory.common.test01;
+
+/**
+ * 本类用来演示:
+ *
+ * @author: success
+ * @date: 2020/8/3 9:58 上午
+ */
+public class WXSender implements Sender{
+    public WXSender(){
+        System.out.println("WXSender...");
+    }
+}
