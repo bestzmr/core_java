f96d93e7b02b7aea2c58dcd297eae6a4c77b2cc8
guancg <849962874@qq.com>
2020-08-10 15:44:32 +0800

    蛇

--- src/tech/aistar/snake/StartSnake.java
+++ src/tech/aistar/snake/StartSnake.java
@@ -1,5 +1,7 @@
 package tech.aistar.snake;
 
+import java.util.Scanner;
+
 /**
  * 本类用来演示:开始游戏
  *
@@ -8,6 +10,36 @@ package tech.aistar.snake;
  */
 public class StartSnake {
     public static void main(String[] args) {
+        SnakeGame game = new SnakeGame();
+
+        //为了获取内部类Snake对象
+        SnakeGame.Snake snake = game.getSnake();
+
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            game.printGames();
+            System.out.print("请您输入>");
+            String dirs = sc.nextLine();
+            switch (dirs){
+                case "w":
+                    //向上走..
+                    snake.steps(SnakeGame.Snake.UP);
+                    break;
+                case "s":
+                    //向下走..
+                    snake.steps(SnakeGame.Snake.DOWN);
+                    break;
+                case "a":
+                    //向上走..
+                    snake.steps(SnakeGame.Snake.LEFT);
+                    break;
+                case "d":
+                    //向上走..
+                    snake.steps(SnakeGame.Snake.RIGHT);
+                    break;
+
+            }
+        }
 
     }
 }
